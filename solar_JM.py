551a6052b68b24b0235377fa391f074cbbb19f1e
Evan Ó Catháin <Evan.Keane@gmail.com>
2024-06-10 16:35:26 +0100

    Make plotting optional + extend window
    Make plotting optional. Additionally check times for next 48 hours as opposed to 24.

--- solar_JM.py
+++ solar_JM.py
@@ -5,7 +5,6 @@ Created 31/05/2024
 
 import argparse
 import numpy as np 
-import matplotlib.pyplot as plt
 import astropy.units as u
 from astropy.time import Time
 from astropy.coordinates import get_sun, EarthLocation, AltAz
@@ -19,7 +18,7 @@ def minutes_rounded(dt):
         new_minute += 10 
     return dt.replace(minute=0, second=0, microsecond=0) + timedelta(minutes=new_minute) # Create rounded time
 
-def main (horizon):
+def main (horizon, plot):
   # Set up Observer, Target, and observation time objects
   longitude = -7.9219 * u.deg
   latitude = 53.0950 * u.deg
@@ -32,7 +31,7 @@ def main (horizon):
   
   # Define the observation times, use only UTC
   start_time = minutes_rounded(datetime.now(timezone.utc))
-  end_time = start_time + timedelta(days=1)
+  end_time = start_time + timedelta(days=2)
   delta_t = timedelta(minutes=10)  # time step
   times = np.arange(start_time, end_time, delta_t).astype(datetime)
   
@@ -74,27 +73,29 @@ def main (horizon):
     print('The sun does not rise above the specified horizon today.')
   
   # Plot the results - visualisation aid
-  plt.figure(figsize=(10, 6))
-  plt.plot(astropy_times.datetime, sun_altitude, label='Sun Elevation')
-  plt.xlabel('Time')
-  plt.ylabel('Sun Elevation (degrees)')
-  plt.title('Sun Elevation Over Time')
-  plt.legend()
-  plt.grid(True)
-  plt.xticks(rotation=45)
-  plt.tight_layout()
-  plt.show()
+  if(plot):
+    import matplotlib.pyplot as plt
+    plt.figure(figsize=(10, 6))
+    plt.plot(astropy_times.datetime, sun_altitude, label='Sun Elevation')
+    plt.xlabel('Time')
+    plt.ylabel('Sun Elevation (degrees)')
+    plt.title('Sun Elevation Over Time')
+    plt.legend()
+    plt.grid(True)
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    plt.show()
 
 # Define function to parse command line arguments
 def parse_arguments():
   parser = argparse.ArgumentParser(description='Plot sun elevation over time.') # create argument parser
   parser.add_argument('-horizon', type=float, default=30.0, help='Horizon angle (degrees) for filtering sun elevation') # add argument for horizon
-
+  parser.add_argument('-plot', action='store_true',default=False, help='Plot solar elevation for next 24 hours')
   return parser.parse_args()
 
 # Run script with specified horizon angle
 if __name__ == '__main__':
   args = parse_arguments() # call function to parse command line arguments
-  main(args.horizon) # call main function
+  main(args.horizon, args.plot) # call main function
 
 
